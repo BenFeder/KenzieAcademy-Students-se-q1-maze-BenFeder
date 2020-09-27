b3d955e7c918db88e32908b231f0d8d147220d39
kenzieadmin <kenzieadmin@users.noreply.github.com>
2020-09-27 15:16:30 +0000

    deploy: d92ffe24bc017baae5c4f5037d77f5ca230157df

--- code.js
+++ code.js
@@ -17,34 +17,38 @@ const map = [
   "WWWWWWWWWWWWWWWWWWWWW",
 ];
 
-// Iterate through each row in array
-for (rows of map) {
-  // make each row a section (div) for the maze
-  let mazeRow = document.createElement("div");
-  mazeRow.className = "row";
-  document.body.append(mazeRow);
-  // Turn each cell into a piece of the maze...
-  for (let cell = 0; cell < rows.length; cell++) {
-    if (rows[cell] == "W") {
-      // ...as a wall
-      let mazeWall = document.createElement("div");
-      mazeWall.className = "wall";
-      mazeRow.append(mazeWall);
-    } else if (rows[cell] == " ") {
-      // ...as pathway for player to move
-      let mazePath = document.createElement("div");
-      mazePath.className = "path";
-      mazeRow.append(mazePath);
-    } else if (rows[cell] == "S") {
-      // ...as starting position, the player piece
-      let playerPiece = document.createElement("div");
-      playerPiece.className = "player";
-      mazeRow.append(playerPiece);
-    } else if (rows[cell] == "F") {
-      // ...as finishing spot
-      let finishSpot = document.createElement("div");
-      finishSpot.className = "finish";
-      mazeRow.append(finishSpot);
+createMaze();
+
+function createMaze() {
+  // Iterate through each row in map array
+  for (rows of map) {
+    // make each row a section (div) for the maze
+    let mazeRow = document.createElement("div");
+    mazeRow.className = "row";
+    document.body.append(mazeRow);
+    // Turn each cell into a piece of the maze...
+    for (let cell = 0; cell < rows.length; cell++) {
+      if (rows[cell] == "W") {
+        // ...as a wall
+        let mazeWall = document.createElement("div");
+        mazeWall.className = "wall";
+        mazeRow.append(mazeWall);
+      } else if (rows[cell] == " ") {
+        // ...as pathway for player to move
+        let mazePath = document.createElement("div");
+        mazePath.className = "path";
+        mazeRow.append(mazePath);
+      } else if (rows[cell] == "S") {
+        // ...as starting position, the player piece
+        let playerPiece = document.createElement("div");
+        playerPiece.className = "player";
+        mazeRow.append(playerPiece);
+      } else if (rows[cell] == "F") {
+        // ...as finishing spot
+        let finishSpot = document.createElement("div");
+        finishSpot.className = "finish";
+        mazeRow.append(finishSpot);
+      }
     }
   }
 }
@@ -52,17 +56,71 @@ for (rows of map) {
 playerPiece.addEventListener("keydown", movePiece);
 
 function movePiece(event) {
-  if (event.keyCode == 38 || event.keyCode == 40) {
-    if (event.keyCode == 38) {
-      mazePath.append(playerPiece); // move piece up
-    } else if (event.keyCode == 40) {
-      mazePath.append(playerPiece); // move piece down
+  // update position of "S" in map array, so that the player piece div appends to the next cell over (up, down, left, or right)
+  if (event.keyCode == 38) {
+    // If player hits up arrow key
+    for (rows of map) {
+      if (map[rows].indexOf("S") >= 0) {
+        // If player piece exists in that row
+        let indexPlayer = map[rows].indexOf("S"); // get index number of that player piece
+        if (map[rows - 1][indexPlayer] != "W") {
+          // If next cell up in previous array row isn't a wall
+          map[rows].split("").splice(indexPlayer, 1, " ").join("");
+          map[rows - 1].split("").splice(indexPlayer, 1, "S").join(""); // move piece up
+        }
+      }
     }
-  } else if (event.keyCode == 37 || event.keyCode == 39) {
-    if (event.keyCode == 37) {
-      mazePath.append(playerPiece); // move piece left
-    } else if (event.keyCode == 39) {
-      mazePath.append(playerPiece); // move piece right
+  } else if (event.keyCode == 40) {
+    // If player hits down arrow key
+    for (rows of map) {
+      if (map[rows].indexOf("S") >= 0) {
+        // If player piece exists in that row
+        let indexPlayer = map[rows].indexOf("S"); // get index number of that player piece
+        if (map[rows + 1][indexPlayer] != "W") {
+          // If next cell down in next array row isn't a wall
+          map[rows].split("").splice(indexPlayer, 1, " ").join("");
+          map[rows + 1].split("").splice(indexPlayer, 1, "S").join(""); // move piece down
+        }
+      }
+    }
+  } else if (event.keyCode == 37) {
+    // If player hits left arrow key
+    for (rows of map) {
+      if (map[rows].indexOf("S") >= 0) {
+        // If player piece exists in that row
+        let indexPlayer = map[rows].indexOf("S"); // get index number of that player piece
+        if (map[rows][indexPlayer - 1] != "W") {
+          // If next cell to the left isn't a wall
+          map[rows]
+            .split("")
+            .splice(indexPlayer - 1, 2, "S", " ")
+            .join(""); // move piece left
+        }
+      }
+    }
+  } else if (event.keyCode == 39) {
+    // If player hits right arrow key
+    for (rows of map) {
+      if (map[rows].indexOf("S") >= 0) {
+        // If player piece exists in that row
+        let indexPlayer = map[rows].indexOf("S"); // get index number of that player piece
+        if (
+          map[rows][indexPlayer + 1] != "W" && // If next cell to the right isn't a wall
+          map[rows][indexPlayer + 1] != "F" // If next cell over ISN'T the "Finish" cell
+        ) {
+          map[rows].split("").splice(indexPlayer, 2, " ", "S").join(""); // move piece right
+        } else if (
+          map[rows][indexPlayer + 1] != "W" && // If next cell to the right isn't a wall
+          map[rows][indexPlayer + 1] == "F" // If next cell over is "Finish" cell
+        ) {
+          map[rows].split("").splice(indexPlayer, 2, " ", "S").join("");
+          // winning condition met, display that the player won by moving player piece onto finish cell
+          let displayWin = document.createElement("div");
+          displayWin.innerText =
+            "Congratulations! You won, and are now crowned king of the Seven Kingdoms.";
+          document.body.append(displayWin);
+        }
+      }
     }
   }
 }
