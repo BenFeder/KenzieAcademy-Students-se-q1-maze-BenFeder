d832b1792e22133d189763104e876b70c529dc61
BenFeder <BenjaminTFeder@gmail.com>
2020-09-28 15:23:41 -0400

    Completed maze successfully, can move in any direction, and winning condition diplays message when player reaches finish.

--- code.js
+++ code.js
@@ -17,15 +17,20 @@ const map = [
   "WWWWWWWWWWWWWWWWWWWWW",
 ];
 
+let gameMaze = document.getElementById("gameMaze");
+
+map[9][0] = " ";
+map[9][1] = "S";
 createMaze();
 
 function createMaze() {
   // Iterate through each row in map array
+  gameMaze.innerHTML = "";
   for (rows of map) {
     // make each row a section (div) for the maze
     let mazeRow = document.createElement("div");
     mazeRow.className = "row";
-    document.body.append(mazeRow);
+    gameMaze.append(mazeRow);
     // Turn each cell into a piece of the maze...
     for (let cell = 0; cell < rows.length; cell++) {
       if (rows[cell] == "W") {
@@ -53,57 +58,88 @@ function createMaze() {
   }
 }
 
-playerPiece.addEventListener("keydown", movePiece);
+document.addEventListener("keydown", movePiece);
 
 function movePiece(event) {
   // update position of "S" in map array, so that the player piece div appends to the next cell over (up, down, left, or right)
   if (event.keyCode == 38) {
-    // If player hits up arrow key
-    for (rows of map) {
+    moveUp();
+  }
+  // If player hits up arrow key
+  function moveUp() {
+    for (rows = 0; rows < map.length; rows++) {
       if (map[rows].indexOf("S") >= 0) {
         // If player piece exists in that row
         let indexPlayer = map[rows].indexOf("S"); // get index number of that player piece
         if (map[rows - 1][indexPlayer] != "W") {
           // If next cell up in previous array row isn't a wall
-          map[rows].split("").splice(indexPlayer, 1, " ").join("");
-          map[rows - 1].split("").splice(indexPlayer, 1, "S").join("");
+          let rowItemsTop = map[rows - 1].split("");
+          rowItemsTop[indexPlayer] = "S";
+          map[rows - 1] = rowItemsTop.join("");
+
+          let rowItemsBottom = map[rows].split("");
+          rowItemsBottom[indexPlayer] = " ";
+          map[rows] = rowItemsBottom.join("");
           createMaze(); // move piece up
         }
       }
     }
-  } else if (event.keyCode == 40) {
-    // If player hits down arrow key
-    for (rows of map) {
+  }
+
+  if (event.keyCode == 40) {
+    moveDown();
+  }
+  // If player hits down arrow key
+  function moveDown() {
+    for (rows = 0; rows < map.length; rows++) {
       if (map[rows].indexOf("S") >= 0) {
         // If player piece exists in that row
         let indexPlayer = map[rows].indexOf("S"); // get index number of that player piece
         if (map[rows + 1][indexPlayer] != "W") {
           // If next cell down in next array row isn't a wall
-          map[rows].split("").splice(indexPlayer, 1, " ").join("");
-          map[rows + 1].split("").splice(indexPlayer, 1, "S").join("");
+          let rowItemsTop = map[rows].split("");
+          rowItemsTop[indexPlayer] = " ";
+          map[rows] = rowItemsTop.join("");
+
+          let rowItemsBottom = map[rows + 1].split("");
+          rowItemsBottom[indexPlayer] = "S";
+          map[rows + 1] = rowItemsBottom.join("");
+
           createMaze(); // move piece down
         }
       }
     }
-  } else if (event.keyCode == 37) {
-    // If player hits left arrow key
-    for (rows of map) {
+  }
+
+  if (event.keyCode == 37) {
+    moveLeft();
+  }
+  // If player hits left arrow key
+
+  function moveLeft() {
+    for (rows = 0; rows < map.length; rows++) {
       if (map[rows].indexOf("S") >= 0) {
         // If player piece exists in that row
         let indexPlayer = map[rows].indexOf("S"); // get index number of that player piece
         if (map[rows][indexPlayer - 1] != "W") {
           // If next cell to the left isn't a wall
-          map[rows]
-            .split("")
-            .splice(indexPlayer - 1, 2, "S", " ")
-            .join(""); // move piece left
+          let rowItems = map[rows].split("");
+          rowItems[indexPlayer - 1] = "S";
+          rowItems[indexPlayer] = " ";
+          map[rows] = rowItems.join("");
           createMaze();
         }
       }
     }
-  } else if (event.keyCode == 39) {
-    // If player hits right arrow key
-    for (rows of map) {
+  }
+
+  if (event.keyCode == 39) {
+    moveRight();
+  }
+  // If player hits right arrow key
+
+  function moveRight() {
+    for (rows = 0; rows < map.length; rows++) {
       if (map[rows].indexOf("S") >= 0) {
         // If player piece exists in that row
         let indexPlayer = map[rows].indexOf("S"); // get index number of that player piece
@@ -111,21 +147,30 @@ function movePiece(event) {
           map[rows][indexPlayer + 1] != "W" && // If next cell to the right isn't a wall
           map[rows][indexPlayer + 1] != "F" // If next cell over ISN'T the "Finish" cell
         ) {
-          map[rows].split("").splice(indexPlayer, 2, " ", "S").join("");
+          let rowItems = map[rows].split("");
+          rowItems[indexPlayer] = " ";
+          rowItems[indexPlayer + 1] = "S";
+          map[rows] = rowItems.join("");
           createMaze(); // move piece right
         } else if (
           map[rows][indexPlayer + 1] != "W" && // If next cell to the right isn't a wall
           map[rows][indexPlayer + 1] == "F" // If next cell over is "Finish" cell
         ) {
-          map[rows].split("").splice(indexPlayer, 2, " ", "S").join("");
-          createMaze();
-          // winning condition met, display that the player won by moving player piece onto finish cell
-          let displayWin = document.createElement("div");
+          let displayWin = document.getElementById("displayWin");
+          displayWin.className = "displayWin";
           displayWin.innerText =
             "Congratulations! You won, and are now crowned king of the Seven Kingdoms.";
           document.body.append(displayWin);
+          // winning condition met, display that the player won by moving player piece onto finish cell
+          let rowItems = map[rows].split("");
+          rowItems[indexPlayer] = " ";
+          rowItems[indexPlayer + 1] = "S";
+          map[rows] = rowItems.join("");
+          createMaze();
         }
       }
     }
   }
 }
+
+// Citation: Thanks to Mike A. for giving me guidance on maze
